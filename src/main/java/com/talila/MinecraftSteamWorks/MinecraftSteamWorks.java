6de1598307ff535d9a86aa9680623a7604f8665c
Kyle Lopez <kylealopez@gmail.com>
2014-06-27 01:05:02 -0700

    Modified Build script and created Master mod file

---
+++ src/main/java/com/talila/MinecraftSteamWorks/MinecraftSteamWorks.java
@@ -0,0 +1,30 @@
+package com.talila.MinecraftSteamWorks;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Talila on 6/27/2014.
+ */
+@Mod(modid="MinecraftSteamWorks", name="Minecraft SteamWorks", version="1.7.2-1.0")
+public class MinecraftSteamWorks {
+   @Mod.Instance("MinecraftSteamWorks")
+   public static MinecraftSteamWorks instance;
+
+   @Mod.EventHandler
+   public void preInit(FMLPreInitializationEvent event) {
+
+   }
+
+   @Mod.EventHandler
+   public void init(FMLInitializationEvent event) {
+
+   }
+
+   @Mod.EventHandler
+   public void postInit(FMLPostInitializationEvent event) {
+
+   }
+}
