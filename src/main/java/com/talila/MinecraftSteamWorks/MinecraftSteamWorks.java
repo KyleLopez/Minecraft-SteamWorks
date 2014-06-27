464bba2cd9c3f59e594ed8804080e86140c2578c
Kyle Lopez <kylealopez@gmail.com>
2014-06-27 02:10:04 -0700

    Relocated files, added Reference, Utility and Proxies

--- src/main/java/com/talila/MinecraftSteamWorks/MinecraftSteamWorks.java
+++ src/main/java/com/talila/MinecraftSteamWorks/MinecraftSteamWorks.java
@@ -1,6 +1,9 @@
-package com.talila.MinecraftSteamWorks;
+package com.talila.minecraftsteamworks;
 
+import com.talila.minecraftsteamworks.proxy.IProxy;
+import com.talila.minecraftsteamworks.reference.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -8,10 +11,14 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 /**
  * Created by Talila on 6/27/2014.
  */
-@Mod(modid="MinecraftSteamWorks", name="Minecraft SteamWorks", version="1.7.2-1.0")
+@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
 public class MinecraftSteamWorks {
-   @Mod.Instance("MinecraftSteamWorks")
+   @Mod.Instance("minecraftsteamworks")
    public static MinecraftSteamWorks instance;
+
+   @SidedProxy(clientSide = Reference.CLIENT_PROXY,
+           serverSide = Reference.SERVER_PROXY)
+   public static IProxy proxy;
 
    @Mod.EventHandler
    public void preInit(FMLPreInitializationEvent event) {
---
+++ src/main/java/com/talila/MinecraftSteamWorks/proxy/ServerProxy.java
@@ -0,0 +1,7 @@
+package com.talila.minecraftsteamworks.proxy;
+
+/**
+ * Created by Talila on 6/27/2014.
+ */
+public class ServerProxy extends CommonProxy {
+}
